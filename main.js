6bdb77afb21ad9b1e28b93f3cd5575d3b1345f54
Nicola <nginvernizzi1@gmail.com>
2020-10-27 14:52:26 +0100

    js done

---
+++ main.js
@@ -0,0 +1,48 @@
+// SLIDER
+
+$(document).ready( function() {
+
+    $('.next').click( function() {
+        slideImg('next');
+    });
+    
+    $('.prev').click( function() {
+        slideImg('prev');
+    });
+    
+    
+})
+
+// FUNCTION
+
+function slideImg(direction) {
+    var activeImg = $('.images img.active');
+    var activeCircle = $('.nav i.active');
+
+    activeImg.removeClass('active');
+    activeCircle.removeClass('active');
+    
+    // NEXT
+    if (direction === 'next') {
+
+        if (activeImg.hasClass('last') === true) {
+            $('.images img.first').addClass('active');
+            $('.nav i.first').addClass('active');
+        } else {
+            activeImg.next().addClass('active');
+            activeCircle.next().addClass('active');
+        }
+    }
+
+    // PREV
+    if (direction === 'prev') {
+
+        if (activeImg.hasClass('first') === true) {
+            $('.images img.last').addClass('active');
+            $('.nav i.last').addClass('active');
+        } else {
+            activeImg.prev().addClass('active');
+            activeCircle.prev().addClass('active');
+        }
+    }
+}
